aea3ec4c8cabb4d3e1ab81347fbb95c1f6e87290
Andrii Chychkan <mywork1728@gmail>
2025-12-25 22:06:55 +0200

    added serializer and path

--- author/urls.py
+++ author/urls.py
@@ -1,5 +1,16 @@
 # Create your urls here
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
 
-urlpatterns = []
+from author.views import AuthorViewSet
+
+router = DefaultRouter()
+router.register(r"manage", AuthorViewSet, basename="manage")
+
+
+# The API URLs are now determined automatically by the router.
+urlpatterns = [
+    path("", include(router.urls)),
+]
 
 app_name = "author"
